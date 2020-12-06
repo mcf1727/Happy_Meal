6f3b0e7d13efc50b9e2bd849cbcbdc222f9e6399
Kev <54264775+mcf1727@users.noreply.github.com>
2020-12-06 12:24:56 +0100

    feat: Implement Google Ads Admob + Firebase
    - Admob: display Ads banner on the MainActivity
    - Firebase: add the sdk into the project

--- app/src/main/java/android/example/com/happymeal/MainActivity.java
+++ app/src/main/java/android/example/com/happymeal/MainActivity.java
@@ -15,6 +15,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity implements NutritionAdapter.ListItemClickListener{
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements NutritionAdapter.
 
     private AppDatabase mDb;
     NutritionAdapter mNutritionAdapter;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +56,16 @@ public class MainActivity extends AppCompatActivity implements NutritionAdapter.
             }
         });
         setUpMainViewModel();
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     private void setUpMainViewModel() {
